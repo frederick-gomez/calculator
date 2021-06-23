d49354a249fca35fdf735b0d69a3b3198c283634
Federico Gomez <gomez.fede98@gmail.com>
2021-06-22 23:57:19 -0400

    Basic elements fetch

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const displayNumbers = document.getElementById('display-num'),
+one = document.getElementById('1'),
+two = document.getElementById('2'),
+three = document.getElementById('3'),
+four = document.getElementById('4'),
+five = document.getElementById('5'),
+six = document.getElementById('6'),
+seven = document.getElementById('7'),
+eight = document.getElementById('8'),
+nine = document.getElementById('9'),
+zero = document.getElementById('0'),
+comma = document.getElementById(',');
+
+function renderNumbers() {
+  
+}
+
+one.addEventListener('click', )
