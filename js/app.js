d4a7d20b77296b6a7d15b3c5a26b30f478e2cad8
Federico Gomez <gomez.fede98@gmail.com>
2021-06-24 10:07:06 -0400

    Stuck here

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
 const one = document.getElementById('1'),
-two = document.getElementById('2'),
-three = document.getElementById('3'),
-four = document.getElementById('4'),
-five = document.getElementById('5'),
-six = document.getElementById('6'),
-seven = document.getElementById('7'),
-eight = document.getElementById('8'),
-nine = document.getElementById('9'),
-zero = document.getElementById('0'),
-comma = document.getElementById(',');
+	two = document.getElementById('2'),
+	three = document.getElementById('3'),
+	four = document.getElementById('4'),
+	five = document.getElementById('5'),
+	six = document.getElementById('6'),
+	seven = document.getElementById('7'),
+	eight = document.getElementById('8'),
+	nine = document.getElementById('9'),
+	zero = document.getElementById('0'),
+	comma = document.getElementById(',');
 
-//Works as intended
-function fetchNumbers(num) {
-  let fetchNum = num.innerText;
-  return fetchNum;
-}
 //Can't append new number
 function renderNumbers(num) {
-  const displayNumbers = document.getElementById('display-num').innerText;
-  let currentNum = displayNumbers;
-  let number = fetchNumbers(num);
-  currentNum.appendChild(number);
+	const currentNumbers = document.getElementById('current-num').textContent;
+	let fetchNum = num.value;
+	currentNumbers.textContent = currentNumbers + fetchNum;
 }
 
 one.addEventListener('click', renderNumbers(one));
