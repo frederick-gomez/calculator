bf8c4ed24c9cfb019aa569d9bb7254507d86a810
Nathan G <gomez.fede98@gmail.com>
2021-07-02 22:29:22 -0400

    Calculator working

--- js/app.js
+++ js/app.js
@@ -5,10 +5,82 @@ const deleteButton = document.querySelector('[data-delete]');
 const clearButton = document.querySelector('[data-all-clear]');
 const prevNum = document.getElementById('prev-num');
 const currentNum = document.getElementById('current-num');
+const operand = document.getElementById('operand');
 
-//Can't append new number
-function renderNumbers(num) {
-	const currentNumbers = document.getElementById('current-num').textContent;
-	let fetchNum = num.value;
-	currentNumbers.textContent = currentNumbers + fetchNum;
+//Functions
+
+function clearAll() {
+	prevNum.innerText = '';
+	currentNum.innerText = '';
+	operand.innerText = '';
 }
+
+function appendNumber(number) {
+	if (number === '.' && currentNum.innerText.includes('.')) return;
+	currentNum.innerText += number;
+}
+
+function deleteNum() {
+	currentNum.innerText = currentNum.innerText.toString().slice(0, -1);
+}
+
+function chooseOperation(operation) {
+	if (!currentNum.innerText) return;
+	if (prevNum !== '') {
+		compute();
+		updateDisplay();
+	}
+	prevNum.innerText = currentNum.innerText;
+	operand.innerText = operation;
+	currentNum.innerText = '';
+}
+
+function compute() {
+	let result;
+	const prev = parseFloat(prevNum.innerText);
+	const current = parseFloat(currentNum.innerText);
+	const operation = operand.innerText;
+
+	if (isNaN(prev) || isNaN(current)) return;
+	switch (operation) {
+		case '+':
+			result = prev + current;
+			break;
+		case '-':
+			result = prev - current;
+			break;
+		case '÷':
+			result = prev / current;
+			break;
+		case '×':
+			result = prev * current;
+			break;
+		default:
+			return;
+	}
+	prevNum.innerText = '';
+	currentNum.innerText = result;
+	operand.innerText = '';
+}
+
+function updateDisplay() {}
+
+//Event Listeners
+
+numberButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		appendNumber(button.innerText);
+		updateDisplay();
+	});
+});
+
+operandButtons.forEach((operand) => {
+	operand.addEventListener('click', () => {
+		chooseOperation(operand.innerText);
+		updateDisplay();
+	});
+});
+
+clearButton.addEventListener('click', clearAll);
+equalButton.addEventListener('click', compute);
+deleteButton.addEventListener('click', deleteNum);
